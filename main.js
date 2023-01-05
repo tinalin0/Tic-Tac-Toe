a4ebf43419a6daf83f5af8c209bfd11f096dc1bf
tinalin0 <112659515+tinalin0@users.noreply.github.com>
2023-01-05 13:23:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
 // Set up canvas and context
-let cnv = document.getElementById("pong-canvas");
+let cnv = document.getElementById("tic-tac-canvas");
 let ctx = cnv.getContext("2d");
-cnv.width = 800;
-cnv.height = 700;
+cnv.width = 600;
+cnv.height = 600;
 
+// Variables
+let wins = 0;
+let losses = 0;
+
+let array = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
+// Drawing the Lines
+requestAnimationFrame(drawAll);
+
+function drawAll() {
+    drawBackground();
+
+    requestAnimationFrame(drawAll);
+}
+
+function drawBackground() {
+    // Draw vert line 1
+    ctx.linewidth = 2;
+    ctx.strokestyle = "rgb(255, 255, 255)";
+    ctx.beginPath(); 
+    ctx.moveTo(200, 0);
+    ctx.lineTo(200, 600);
+    ctx.stroke(); // Draw line
+
+    // Draw vert line 2
+    ctx.moveTo(400, 0);
+    ctx.lineTo(400, 600);
+    
+    // Draw hori line 1
+    ctx.moveTo(0, 200);
+    ctx.lineTo(600, 200);
+
+    // Draw hori line 2
+    ctx.moveTo(0, 400);
+    ctx.lineTo(600, 400);
+}
